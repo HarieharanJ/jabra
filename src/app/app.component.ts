90e6461b18d0f452850fbbd11607635007d94215
HarieharanJ <you@example.comharijayachandran006@gmail.com>
2024-09-18 20:37:41 +0530

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import "amazon-connect-streams"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const ccpUrl="https://ej-icc-amazonconnect-dev.my.connect.aws/ccp-v2";
+    connect.core.initCCP(this.ccpDiv.nativeElement,{
+      ccpUrl,
+      loginPopup:false,
+      loginPopupAutoClose:true,
+      softphone:{
+        allowFramedSoftphone:true
+      }
+      
+    })
+  }
   title = 'app';
+  
+  @ViewChild('ccpDiv')
+  public ccpDiv:ElementRef|any
+  
+  
 }
